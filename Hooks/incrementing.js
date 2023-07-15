325e592c0de218148b7821cf8128450f364d9d8c
Vasu Bansal <72257862+Vasu2710@users.noreply.github.com>
2023-07-15 21:00:15 +0530

    Update incrementing.js

--- Hooks/incrementing.js
+++ Hooks/incrementing.js
@@ -1,1 +1,22 @@
 
+
+            //HOOKS
+import React, {useState}from 'react'
+
+function App() {
+  const [counter, setIncrement] = useState(10); //any number 
+  const handleIncrement = () => {
+    setIncrement(counter+2);
+  }
+ 
+  return (
+    <>
+       
+        <h2>{counter}</h2>
+        <button onClick={handleIncrement}>Increment</button>
+        <hr></hr>
+    </>
+  )
+}
+
+export default App
